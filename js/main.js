030b237a91d76be7d473bcc8adfc607da40c139e
roweeena <roe0320@gmail.com>
2021-09-20 22:28:02 +1000

    responsiveness attempt

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+	let stickyNavTop = $('#nav').offset().top;
+
+  const stickyNav = function(){
+    let scrollTop = $(window).scrollTop();
+    if(scrollTop >= stickyNavTop){
+      $('#nav').addClass('sticky');
+    } else {
+      $('#nav').removeClass('sticky');
+    }
+  };
+stickyNav();
+
+$(window).scroll(function(){
+  stickyNav();
+});
+
+})
