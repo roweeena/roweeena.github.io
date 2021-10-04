660854267a2dd9126591aa7eb65f7f33d7cf9cab
roweeena <roe0320@gmail.com>
2021-10-04 23:39:10 +1100

    copied to clipboard function

--- js/main.js
+++ js/main.js
@@ -17,7 +17,25 @@ $(window).scroll(function(){
   stickyNav();
 });
 
-
+//flip card animation
 $(".card").flip();
+
+//copy text to clipboard
+const clipboard = new ClipboardJS('#email', {
+        text: function () {
+          return $('#email').text();
+        },
+      });
+
+      clipboard.on('success', function (e) {
+				alert(`Email has been copied to clipboard`)
+        console.log(e);
+      });
+
+      clipboard.on('error', function (e) {
+        console.log(e);
+      });
+
+
 
 })
