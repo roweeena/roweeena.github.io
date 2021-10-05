f3f6122270202c6b2b958ebb91b8d8a6be3d2b60
roweeena <roe0320@gmail.com>
2021-10-05 14:48:05 +1100

    typing animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 
 
@@ -36,6 +37,50 @@ const clipboard = new ClipboardJS('#email', {
         console.log(e);
       });
 
+	//typing effect
+
+	let typeText = $('#text'); //querySelector
+	const textToBeTyped = "But you can call me Ro";
+	const sentenceArray = ["But you can call me Ro", "I'm a junior software engineer", "Keen to grow and learn"];
+	let index = 0, isAdding = true, textToBeTypedIndex = 0;
+
+	const playAnim = function() {
+	setTimeout(function () {
+		const sentence = sentenceArray[textToBeTypedIndex].slice(0, index);
+		typeText.text(sentence)
+		if (isAdding) {
+			// adding text
+			if (index > sentenceArray[textToBeTypedIndex].length) {
+				// no more text to add
+				isAdding = false
+				//break: wait 2s before playing again
+				setTimeout(function () {
+					playAnim()
+				}, 2000)
+				return
+			} else {
+				// increment index by 1
+				index++
+			}
+		} else {
+			// removing text
+			if (index === 0) {
+				// no more text to remove
+				isAdding = true
+				//switch to next text in text array
+				textToBeTypedIndex = (textToBeTypedIndex + 1) % sentenceArray.length
+			} else {
+				// decrement index by 1
+				index--
+			}
+		}
+		// call itself
+		playAnim()
+	}, isAdding ? 120 : 60)
+	}
+	// start animation
+	playAnim()
 
 
+	//end document ready
 })
